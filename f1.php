e7b981f3c148bb2d092fa870a40c42136226844d
cagycee <71255514+cagycee@users.noreply.github.com>
2020-12-06 01:53:46 -0600

    ENCRYPTION AND DECRYPTION
    Added a way to encrypt and decrypt data securely with functions encrypt() and decrypt()

--- f1.php
+++ f1.php
@@ -1,10 +1,15 @@
 <?php
-//cLibrary initially started 12/3/2020 by Serra Jones(cagycee).
+//cLibrary PHP initially started 12/3/2020 by Serra Jones(cagycee).
 class cLibrary {
     public static $version = '0.1.0a';
     private $coninfo;
+    private $cp;
+    private $dp;
     public function sqlcon($h=null, $u=null, $p=null, $d=null, $c=null) {
-        // CONNECTS TO DATABASE USING PDO
+        /* CONNECTS TO DATABASE USING PDO 
+        *  EX: sqlcon(yourhost, youruser, yourpass, yourdatabase, yourcharset)
+        *  Localhost: $var = $clvar->sqlcon(null, null, null, "DATABASE", null);
+        */ 
         $this->coninfo = Array(
             'host'=> ($h ? $h : '127.0.0.1'),
             'username' => ($u ? $u : 'root'),
@@ -21,5 +26,51 @@ class cLibrary {
             echo 'Connection Failed: '. $e->getMessage();
         }
     }
+
+    public function encrypt($str, $key, $iM=null, $i=null, $o=null) {
+        /* ENCRYPTS ANY DATA USING OPENSSL
+         * EX: $encrypted = $cc->encrypt('THISISASTRING', 'BLAHBLAHKEYTHIS');
+         * NOTE: By changing the IV method and/or IV number, your encryption will become more secure! 
+         * Reassure that you are using the same method, key, option, and iv for the counterpart function decrypt()!
+         * FULL USE: $variable = $clvar->encrypt('THISISASTRING', 'BLAHBLAHKEYTHIS', 'aes-128-cbc', '6584692267521658', null);
+         */
+        $this->cp = Array(
+            'method' => ($iM ? $iM : 'AES-128-CTR'),
+            'iv' => ($i ? $i : '1234567891012345'),
+            'option' => ($o ? $o : 0),
+            'str' => $str,
+            'key' => $key
+        );
+        if ($str===null) {
+            throw new Exception('No string to encrypt');
+        } else if ($key===null) {
+            throw new Exception('Cannot encrypt '.$str. ' wihtout key');
+        } else {
+            return openssl_encrypt($this->cp['str'], $this->cp['method'], $this->cp['key'], $this->cp['option'], $this->cp['iv']);
+        }
+    }
+
+    public function decrypt($enc, $key, $iM=null, $i=null, $o=null) {
+        /* DECRYPTS a specific ENCRYPTION USING OPENSSL. REQUIRED TO KNOW ENCRYPTION's IV METHOD, IV #, OPTION, and KEY
+         * EX: $encrypted = $cc->decrypt('THISISASTRING', 'BLAHBLAHKEYTHIS'); #ONLY IF YOU HAVEN'T PROVIDED the IV, 
+         * Reassure that you are using the same method, key, option, and iv for the counterpart function encrypt()!
+         * FULL USE: $variable = $clvar->decrypt('vmUqpYqnk66FoPwIllAbpg==', 'BLAHBLAHKEYTHIS', 'aes-128-cbc', '6584692267521658', null);
+         */
+        $this->dcp = Array(
+            'method' => ($iM ? $iM : 'AES-128-CTR'), //iv method
+            'iv' => ($i ? $i : '1234567891012345'), // iv #
+            'option' => ($o ? $o : 0), //option
+            'enc' => $enc, //encryption
+            'key' => $key // key
+        );
+        if ($enc===null) {
+            throw new Exception('No encryption to decrypt');
+        } else if ($key===null) {
+            throw new Exception('Cannot decrypt '.$enc. ' wihtout key');
+        } else {
+            return openssl_decrypt($this->dcp['enc'], $this->dcp['method'], $this->dcp['key'], $this->dcp['option'], $this->dcp['iv']);
+        }
+    }
+
 }
 ?>
